f0c8a8f5bf88b397cf9066a68554267ed79e7f2b
Jake Zappin <jake.zappin@gmail.com>
2016-09-29 15:23:11 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,43 @@
+class Book
 
+  def initialize(title)
+    @title = title
+  end
 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+  def title=(title)
+    @title = title
+  end
+
+  def title
+    @title
+  end
+
+  def author=(name)
+    @author = name
+  end
+
+  def author
+    @author
+  end
+
+  def page_count=(num)
+    @page_count = num
+  end
+
+  def page_count
+    @page_count
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+
+end
